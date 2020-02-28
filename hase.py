c48565fd0c4df5963b9f17c5f505b4681a0b82d3
Ric <det@web.de>
2020-02-28 17:42:48 +0100

    python opens styles

--- hase.py
+++ hase.py
@@ -1,1 +1,12 @@
+import sys
+
 print ("HELLO THERE")
+
+try:
+    d = open("styles.txt")
+except:
+    print("Datei komisch")
+    sys.exit(0)
+allezeilen = d.readlines()
+d.close()
+print(allezeilen)
