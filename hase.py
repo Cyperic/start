799342a628a3b27c19ef2060effe004f98d5bdcc
Ric <det@web.de>
2020-03-01 16:29:36 +0100

    Relaxing hound + Counter created

--- hase.py
+++ hase.py
@@ -4,6 +4,7 @@ print ("stylish cool hound")
 
 try:
     d = open("styles.txt")
+    e = open("Count.txt","r+")
 except:
     print("Datei komisch")
     sys.exit(0)
@@ -11,4 +12,14 @@ allezeilen = d.readlines()
 d.close()
 for i in allezeilen:
     print(i,end ="")
+
+zeile1 = e.readline()
+new = int(zeile1)+1
+print("\nCount:",new)
+e.write(str(new))
+e.close()
+
+
+
+    
 
